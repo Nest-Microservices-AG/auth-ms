7fa2e78cde448dc155e230ce7eb52b416ac39f57
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2024-07-23 19:09:46 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# usersAuth-ms
---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { envs } from './config';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  console.log('server running on port' + envs.port);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.natsServer,
+      },
+    },
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  console.log('asdasdasdasd');
+  await app.listen();
+}
+bootstrap();
